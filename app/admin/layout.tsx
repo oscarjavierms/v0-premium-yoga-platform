073676b43f4996eee8ac5f463387c4a732d93134
oscarjavierms <oscarjavierms@gmail.com>
2026-01-19 23:28:14 -0500

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,7 +4,7 @@ import type React from "react"
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-muted/30 flex">
-      <AdminSidebar />
+      
       <main className="flex-1 overflow-auto">{children}</main>
     </div>
   )
